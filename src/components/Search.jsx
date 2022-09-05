229c68edd76d2d025df35784b2729f3496a72fbc
Cleifson Araujo <cl.raujo1@gmail.com>
2022-09-04 22:56:12 -0300

    Search bar styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
-  return <div>Search</div>;
+  const [input, setInput] = useState('');
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate('/searched/' + input);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className='form'>
+      <div className='form__search'>
+        <FaSearch />
+        <input
+          className='form__input'
+          type='text'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default Search;
